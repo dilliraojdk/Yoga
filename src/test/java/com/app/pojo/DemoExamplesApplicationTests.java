c858c4a53f7142cdf6294d9867c4c9358a4a1130
TL <TL@gmail.com>
2024-08-15 13:07:17 +0530

    message  spring boot examples

---
+++ src/test/java/com/app/pojo/DemoExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.pojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
